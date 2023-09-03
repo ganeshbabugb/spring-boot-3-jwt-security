a6d14eaf8384ee4ba9387b1656f358024dd0d633
Ganesh babu <ganeshbabugb2525@gmail.com>
2023-09-03 15:11:30 +0530

    Fixed: Indent, In addition changed project name.

--- src/main/java/com/alibou/security/models/token/TokenType.java
+++
@@ -1,5 +0,0 @@
-package com.alibou.security.models.token;
-
-public enum TokenType {
-  BEARER
-}
---
+++ src/main/java/com/ganesh/security/models/token/TokenType.java
@@ -0,0 +1,5 @@
+package com.ganesh.security.models.token;
+
+public enum TokenType {
+    BEARER
+}
