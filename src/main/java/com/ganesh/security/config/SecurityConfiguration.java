43b92e48d0a81f57b11a7486284ce52f553b963e
Ganesh babu <ganeshbabugb2525@gmail.com>
2023-09-04 20:34:29 +0530

    Commented annotation based permission

--- src/main/java/com/ganesh/security/config/SecurityConfiguration.java
+++ src/main/java/com/ganesh/security/config/SecurityConfiguration.java
@@ -53,20 +53,17 @@ public class SecurityConfiguration {
 
                 .requestMatchers("/api/v1/management/**").hasAnyRole(ADMIN.name(), MANAGER.name())
 
-
                 .requestMatchers(GET, "/api/v1/management/**").hasAnyAuthority(ADMIN_READ.name(), MANAGER_READ.name())
                 .requestMatchers(POST, "/api/v1/management/**").hasAnyAuthority(ADMIN_CREATE.name(), MANAGER_CREATE.name())
                 .requestMatchers(PUT, "/api/v1/management/**").hasAnyAuthority(ADMIN_UPDATE.name(), MANAGER_UPDATE.name())
                 .requestMatchers(DELETE, "/api/v1/management/**").hasAnyAuthority(ADMIN_DELETE.name(), MANAGER_DELETE.name())
 
+                .requestMatchers("/api/v1/admin*").hasRole(ADMIN.name())
 
-                /* .requestMatchers("/api/v1/admin/**").hasRole(ADMIN.name())
-
-                 .requestMatchers(GET, "/api/v1/admin/**").hasAuthority(ADMIN_READ.name())
-                 .requestMatchers(POST, "/api/v1/admin/**").hasAuthority(ADMIN_CREATE.name())
-                 .requestMatchers(PUT, "/api/v1/admin/**").hasAuthority(ADMIN_UPDATE.name())
-                 .requestMatchers(DELETE, "/api/v1/admin/**").hasAuthority(ADMIN_DELETE.name())*/
-
+                 .requestMatchers(GET, "/api/v1/admin*").hasAuthority(ADMIN_READ.name())
+                 .requestMatchers(POST, "/api/v1/admin*").hasAuthority(ADMIN_CREATE.name())
+                 .requestMatchers(PUT, "/api/v1/admin*").hasAuthority(ADMIN_UPDATE.name())
+                 .requestMatchers(DELETE, "/api/v1/admin*").hasAuthority(ADMIN_DELETE.name())
 
                 .anyRequest()
                 .authenticated()
--- src/main/java/com/ganesh/security/controller/AdminController.java
+++ src/main/java/com/ganesh/security/controller/AdminController.java
@@ -1,37 +1,35 @@
 package com.ganesh.security.controller;
 
-import io.swagger.v3.oas.annotations.Hidden;
-import org.springframework.security.access.prepost.PreAuthorize;
+//import io.swagger.v3.oas.annotations.Hidden;
+//import org.springframework.security.access.prepost.PreAuthorize;
+
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/admin")
-@PreAuthorize("hasRole('ADMIN')")
+//@PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
 
     @GetMapping
-    @PreAuthorize("hasAuthority('admin:read')")
+//    @PreAuthorize("hasAuthority('admin:read')")
     public String get() {
         return "GET:: admin controller";
     }
 
     @PostMapping
-    @PreAuthorize("hasAuthority('admin:create')")
-    @Hidden
+//    @PreAuthorize("hasAuthority('admin:create')")
     public String post() {
         return "POST:: admin controller";
     }
 
     @PutMapping
-    @PreAuthorize("hasAuthority('admin:update')")
-    @Hidden
+//    @PreAuthorize("hasAuthority('admin:update')")
     public String put() {
         return "PUT:: admin controller";
     }
 
     @DeleteMapping
-    @PreAuthorize("hasAuthority('admin:delete')")
-    @Hidden
+//    @PreAuthorize("hasAuthority('admin:delete')")
     public String delete() {
         return "DELETE:: admin controller";
     }
